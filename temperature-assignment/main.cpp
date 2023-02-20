a732992d4aeafed8718f8e204afd345b0826d3a8
vange <nahui@miquizt.li>
2023-02-20 10:21:25 -0800

    it works for the assignment

--- temperature-assignment/main.cpp
+++ temperature-assignment/main.cpp
@@ -5,24 +5,35 @@ using namespace std;
 int main()
 {
 
-	cout << "Hello, World!\n";
+//	cout << "Hello, World!\n";
+/* variables */
 	char my_char;
 	int start_num;
 	int stop_num;
 	int step_num;
 	double cel;
-
-	cout << "enter :";
+	double fah;
+// # prompt
+// input Fahrenheit start, stop, and step values
+	cout << "enter start Fahrenheit: ";
 	cin >> start_num;
-	cout << "enter :";
+	cout << "enter stop Fahrenheit: ";
 	cin >> stop_num;
-	cout << "enter :";
+	cout << "enter step: ";
 	cin >> step_num;
 	
-	for (int fah = start_num; fah <= stop_num; fah += step_num)
+	cout << "fah" << "	" << "cel" << endl;
+	cout << "---" << "	" << "---" << endl;
+	
+// * set precision command
+
+// ## display loop
+	for (fah = start_num; fah <= stop_num; fah += step_num)
 	{
+// ## conversion formula
 		cel = 5.0 * (fah - 32) / 9;
-		cout << fah << "celsius is " << cel;
+// ## display formatted fah and cel values
+		cout << fah << "	" << cel << endl;
 	}
 	cout << my_char << endl;
 	
